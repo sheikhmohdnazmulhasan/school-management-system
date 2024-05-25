e6bb5d357bc77be85d08fadd358274ecee92783b
mdnazmul42726 <nazmulofficial@outlook.com>
2024-05-25 20:11:15 +0600

    Refactor User Validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,12 +2,9 @@ import { z } from "zod";
 // import { TUser } from "./user.interface";
 
 const userSchemaValidation = z.object({
-    id: z.string(),
-    password: z.string().max(20, { message: 'Password can not be more then 20 character' }),
-    needsPasswordChanges: z.boolean().optional().default(true),
-    role: z.enum(['admin', 'faculty', 'student']),
-    status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-    isDeleted: z.boolean().optional().default(false)
+    password: z.string({
+        invalid_type_error: 'Password must be string'
+    }).max(20, { message: 'Password can not be more then 20 character' }).optional()
 
 });
 
