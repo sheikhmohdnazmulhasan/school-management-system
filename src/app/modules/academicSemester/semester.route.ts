d59336f804f417a61448df5aa350e25c46089dbd
mdnazmul42726 <nazmulofficial@outlook.com>
2024-06-30 15:45:01 +0600

    validate academic semester payload with zod

--- src/app/modules/academicSemester/semester.route.ts
+++ src/app/modules/academicSemester/semester.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { AcademicSemesterControllers } from "./semester.controller";
+import ValidateRequest from '../../middlewares/zodValidation';
+import { AcademicSemesterValidation } from './semester.validation';
 
 const router = express.Router();
 
-router.post('/create-academic-semester', AcademicSemesterControllers.createAcademicSemester);
+router.post('/create-academic-semester', ValidateRequest(AcademicSemesterValidation.createAcademicSemesterSchema), AcademicSemesterControllers.createAcademicSemester);
 
 export const AcademicSemesterRoutes = router;
