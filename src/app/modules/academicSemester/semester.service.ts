a9094eeddaddf54e031b8f2ba18e6e6b036c223c
mdnazmul42726 <nazmulofficial@outlook.com>
2024-06-30 14:10:22 +0600

    semester service is created

---
+++ src/app/modules/academicSemester/semester.service.ts
@@ -0,0 +1,12 @@
+import httpStatus from "http-status";
+import { TAcademicSemester } from "./semester.interface";
+
+async function createAcademicSemesterIntoDb(data: TAcademicSemester) {
+
+    try {
+        return { status: httpStatus.OK, success: true, message: 'Student Created Successfully', data: data, error: null };
+
+    } catch (error) {
+        return { status: httpStatus.BAD_REQUEST, success: false, message: 'Student Creation Failed', data: null, error: error }
+    };
+};
