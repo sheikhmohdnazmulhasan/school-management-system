41c3563124e4b59636409ddf8912ce8ab21aed5e
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-01 00:40:33 +0600

    academic faculty model created

---
+++ src/app/modules/academicFaculty/faculty.validation.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const academicFacultySchemaValidation = z.object({
+    name: z.string({
+        invalid_type_error: 'Name must be an String'
+    }),
+
+});
+
+export const academicFacultyValidation = { academicFacultySchemaValidation }
