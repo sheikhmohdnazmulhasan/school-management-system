7629b9c8d22f5378d0c3465f4b1c7ebb202b8b60
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-02 15:05:08 +0600

    before Transaction & Rollback

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -8,14 +8,11 @@ async function findLastStudentId() {
 };
 
 export async function generateStudentId(payload: TAcademicSemester) {
-
     const currentId = await findLastStudentId() || (0).toString();
    
     let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
-
     incrementId = `${payload.year}${payload.code}${incrementId}`;
 
-    console.log(currentId);
     return incrementId;
 
 };
